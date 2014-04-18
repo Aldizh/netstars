2897bb087e721bee6de7a2bc744ae4d9d8515fe0
bhuten <tenzin@berkeley.edu>
2014-04-18 18:52:46 -0500

    more changes

--- includes-in/portal-nav.php
+++ includes-in/portal-nav.php
@@ -1,5 +1,6 @@
 <ul class="nav nav-pills nav-stacked">
   <li><a href="home.php"><span class="glyphicon glyphicon-home"> Home</a></li>
-  <li><a href="my-network.php"><span class="glyphicon glyphicon-globe"> My Network</a></li>
+  <li><a href="my-network.php"><span class="glyphicon glyphicon-globe"> My-Network</a></li>
   <li><a href="refer.php"><span class="glyphicon glyphicon-hand-left"> Refer</a></li>
+  <li><a href="ads.php"><span class="glyphicon glyphicon-picture"> Ad-Click</a></li>
 </ul>
