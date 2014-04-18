5d9b766c41aeac2a6b99ed7fe8433ffae8db4843
bhuten <tenzin@berkeley.edu>
2014-04-18 18:46:28 -0500

    ads done

---
+++ portal/ads.php
@@ -0,0 +1,22 @@
+<?php include("../includes-in/header.php"); ?>
+
+<div class="row portal-container">
+	<section class="col-lg-3 col-md-3 member-nav">
+		<?php include("../includes-in/portal-nav.php");?>
+	</section>
+	<section class="col-lg-9 col-md-9 col-sm-9">
+		<ul class="list-inline">
+			<li><img src="../images/portal-ad-sample-1.JPG" width="300" height="300" alt="Portal Ad Sample 1"></li>
+			<li><img src="../images/portal-ad-sample-2.jpg" width="300" height="300" alt="Portal Ad Sample 2"></li>
+			<li><img src="../images/portal-ad-sample-3.jpg" width="300" height="300" alt="Portal Ad Sample 3"></li>
+			<li><img src="../images/portal-ad-sample-4.jpg" width="300" height="300" alt="Portal Ad Sample 4"></li>
+			<li><img src="../images/portal-ad-sample-5.jpg" width="300" height="300" alt="Portal Ad Sample 5"></li>
+			<li><img src="../images/portal-ad-sample-6.jpg" width="300" height="300" alt="Portal Ad Sample 6"></li>
+		</ul>
+		<div class='a' style="width:200px; height:50px; position: fixed;"><a href="#" class="btn btn-primary btn-lg">CLICK HERE</a></div>
+		
+	</section>
+</div>
+			
+			
+<?php include('../includes-in/footer.php'); ?>
