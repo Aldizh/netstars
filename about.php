c581c9830ea8d7f110fdac68bcdc11e864ae72dc
bhuten <tenzin@berkeley.edu>
2014-04-22 17:02:49 -0500

    about us modified

--- about.php
+++ about.php
@@ -14,29 +14,43 @@
 	</h1>
 </div>
 <div class="row word-from-ceo">
+		<div class="col-lg-12">
+			<p>NeXXStars is a global company comprised of “Brand Partners” who have joined our organization as specialized independent direct sales professionals who bring the world of; digital online marketing/advertising, technology and telecommunications to market. </p>
+
+			<p>NeXXStars is the first market system that combines the world's biggest trends in: Internet advertising digital technology, telecom, and useful digital products and services including; Security packages, VOIP, Virtual “Cloud” based PBX, Special Discounted Full-Services Cell Phone, and more!</p>
+
+			<p>We have recruited the best professionals in the areas of business, legal, technology, digital marketing, sales, and support to offer you a fantastic experience in the journey to build a lucrative life-changing career as one of our trusted NeXXStars' "Brand Partners”.</p>
+		</div>
+</div>
+<div class="row word-from-ceo">
+	<h2 class="text-center">THE TEAM</h2>
 		<div class="col-lg-3 col-md-4 text-right hidden-xs">
 			<img src="images/brad.png" alt="Brad Cooper, CEO">
 		</div>
 		<div class="col-lg-9 col-md-8">
-			<h1>A Word From Our CEO</h1>
-			<p>Hello Friends, 
+			<h4><strong>Brad Cook, CEO</strong></h4>
+			<p>Mr. Cook’s first exposure to network marketing was at aged 11 when his Mother and Father joined “Pre-Paid legal”.</p>
+			<p>He quickly grasped the nuances of this business marketing model and by his 16th birthday he signed his first contract with Amway and thus launched a career built upon passion, integrity and drive toward a life of freedom and opportunity!</p>
 
-<p>Nexxstars is a global company that specializes in direct sales and managing of online marketing, technology and telecommunications. We are launching the first system that combines the world's biggest trends: the internet, advertising technology, communication and credit cards with cash back and cash rewards. </p>
-
-<p>We have the best professionals in the areas of business,  legal, technology and support to offer you a fantastic experience and help you earn a career as one of our trusted Partners.</p>
-
-			<h2>About Mr. Cook</h2>
-			<p>Mr. Cook’s first exposure to network marketing was at aged 11 when his Mother and Father joined “Pre-Paid legal”. He quickly grasped the nuances of this business marketing model and by his 16th birthday he signed his first contract with Amway and thus launched a career built upon passion, integrity and drive toward a life of freedom and opportunity! </p>
+			<p><strong>Professional Background Successes: </strong></p>
 			<ul>
-				<li>International Studies Major at The University of Arkansas Little Rock.</li>
-			<li>National Marketing Director:  NSA (National Safety Associates)1989</li>
-			<li>Top ranked in multiple Network Marketing organizations.</li>
-			<li>“Family of Eagles” builder of 250,000 businesses in over 18 countries.</li>
-			<li>2000-2003 Smart Travel “Ambassador” (Presidents Advisory Board) #3 top money earner</li>
-			<li>2004-2009 International Marketing Director - Enliven, International, Inc. 50,000 plus reps; # 2 money earner</li>
-			<li>2009-2011 World Ventures </li>
-			<li>Territory Manager for Reliant Energy</li>
+				<li>Family of Eagles” builder of 250,000 businesses in over 18 countries.</li>
+				<li>National Marketing Director: NSA (National Safety Associates)</li>
+				<li>Top ranked in multiple Network Marketing organizations.</li>
+				<li>Televest – Negotiated land, Roof top, and Utility co-location agreements for AT&T, TMobile, American Tower</li>
+				<li>Vice President - MLM Internet technology company</li>
+				<li>Smart Travel “Ambassador” (Presidents Advisory Board) #3 top money earner</li>
+				<li>Global Signal – Document review for wireless partners</li>
+				<li>International Marketing Director - Enliven, International, Inc. 50,000 plus reps; # 2 money earner</li>
+			</ul>
+			<p><strong>Personal: </strong></p>
+			<ul>
+				<li>Brad is not only a world class network marketeer but a man of many talents and hobbies. A native of Little Rock, Brad attended </li>
+				<li>The University of Arkansas Little Rock with an International Studies Major. He and his Wife live in Dallas with their 2 Daughters.</li>
+				<li>World class long distance runner; Ballroom Dance Instructor</li>
+				<li>Avid traveler , Brad has lived in 3 countries in efforts to learn the most he can about the culture. </li>
 			</ul>
 		</div>
 </div>
+
 <?php include('includes/footer.php'); ?>
